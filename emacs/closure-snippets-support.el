f22e79689fd485c9ccb87c9eed97c89d460446d0
subtilior <dev@possente.net>
2016-07-28 21:18:28 -0400

    Add support for goog.module declarations

--- emacs/closure-snippets-support.el
+++ emacs/closure-snippets-support.el
@@ -1,8 +1,16 @@
+;;; -*- lexical-binding: t -*-
 (defun get-current-provide-string ()
-  "Returns the first goog.provide() string in the current buffer, or nil if not found."
+  "Returns the name of the first provided package in the current buffer, or nil if not found."
   (save-excursion
     (beginning-of-buffer)
-      (when (re-search-forward "goog\.provide *( *[\"']\\([A-Za-z0-9.]+\\)[\"'] *)" nil t)
-        (match-string-no-properties 1))))
+    (when (re-search-forward "goog\.\\(module\\|provide\\) *( *[\"']\\([A-Za-z0-9.]+\\)[\"'] *)" nil t)
+      (let ((keyword (match-string-no-properties 1))
+            (package-name (match-string-no-properties 2)))
+        ;; For goog.module, if the module name is namespaced using periods, the
+        ;; assumption is that the module exports the symbol whose name matches
+        ;; the substring after the last period.
+        (if (string= keyword "module")
+            (substring package-name (string-match "\\([A-Za-z0-9]+\\)$" package-name))
+          package-name)))))
 
 (provide 'closure-snippets-support)
